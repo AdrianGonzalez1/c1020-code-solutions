b8bd23927b874da2df52d10f9554311e7e046a06
Adrian Gonzalez <adrian131@me.comgit>
2020-10-15 20:59:18 -0700

    Re-committed javascript-loops-practice exercise.

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,1 +1,8 @@
 /* exported countdown */
+function countdown(number) {
+  var array = [];
+  for (number; number >= 0; number--) { // starts loop @ number, as long as number is greater than or equal to 0 it will decremeent value of number by 1
+    array.push(number);
+  }
+  return array;
+}
--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -1,1 +1,12 @@
+/* eslint-disable valid-typeof */
 /* exported filterOutStrings */
+function filterOutStrings(values) {
+  var arr = [];
+  var number = typeof 8;
+  for (var i = 0; i < values.length; i++) {
+    if (typeof values[i] === number) {
+      arr.push(values[i]);
+    }
+  }
+  return arr;
+}
