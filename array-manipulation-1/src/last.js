6a3fbfde02efea3074a9459092dfae9fa2ca11eb
Adrian Gonzalez <adrian131@me.comgit>
2020-10-19 10:56:08 -0700

    PR Revision last.js

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,11 +1,4 @@
 /* eslint-disable no-unused-vars */
 function last(array) {
-  for (var i = 0; i < array.length; i++) {
-    return array[array.length - 1];
-  }
-  return undefined;
+  return array[array.length - 1];
 }
-
-// create a for loop to iterate through the length of the array variable
-// target the last element using length property - 1 and return the result
-// return undefined if no value is within the array variable
