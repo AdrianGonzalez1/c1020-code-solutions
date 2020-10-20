9bd559e272ec1ea6058d0d8b67d605a1004d8dfd
Adrian Gonzalez <adrian131@me.comgit>
2020-10-20 16:11:15 -0700

    PR Revisions.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,24 +1,20 @@
 var openModalButton = document.querySelector('.open-modal');
-var openModal = false;
 var overlayElement = document.querySelector('.overlay');
 var modalElement = document.querySelector('.modal');
 var modalButton = document.querySelector('.modal-button');
-var closeModal = false;
 
 openModalButton.addEventListener('click', modalHandle);
 modalButton.addEventListener('click', modalButtonHandle);
 
 function modalHandle(event) {
-  openModal = !openModal;
-  if (openModal) {
+  if (event) {
     overlayElement.className = 'overlay';
     modalElement.className = 'modal';
   }
 }
 
 function modalButtonHandle(event) {
-  closeModal = !closeModal;
-  if (closeModal) {
+  if (event) {
     modalElement.className = 'hidden';
     overlayElement.className = 'hidden';
   }
