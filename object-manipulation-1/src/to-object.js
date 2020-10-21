8904523d678a5f30379f1cf336ec9414983e2bff
Adrian Gonzalez <adrian131@me.comgit>
2020-10-20 18:37:18 -0700

    Completed object-man-1 exercise.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 function toObject(keyValuePair) {
   var obj = {};
-  for (var i = 0; i < obj.length; i++) {
-    obj += keyValuePair;
-  }
+  var one = keyValuePair[0];
+  var two = keyValuePair[1];
+  obj[one] = two;
   return obj;
 }
