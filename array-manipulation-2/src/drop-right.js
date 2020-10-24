d2edd8acf33de9469aaf72ff3115e659c7e694df
Adrian Gonzalez <adrian131@me.comgit>
2020-10-23 20:51:01 -0700

    Completed array-manipulation-2 exercise.

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+function dropRight(array, count) {
+  var arr = [];
+  for (var i = 0; i < array.length - count; i++) {
+    if (array.length !== 0) {
+      arr.push(array[i]);
+    }
+  }
+  return arr;
+}
+
+// create an empty storage array variable
+// iterate through the length of the array variable and stop the iteration at the length minues value in count varaible
+// check if there is value inside array variable
+// if there is push the index to the storage array variable created
+// return storage array variable from the function
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,4 +1,10 @@
 /* eslint-disable no-unused-vars */
 function take(array, count) {
+  var arr = [];
 
+  for (var i = 0; i < array.length; i++) {
+    array.length = count;
+    arr.push(array[i]);
+  }
+  return arr;
 }
