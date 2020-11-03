dd14d1397af08206b05f6bd33360bdba77fe9801
Adrian Gonzalez <adrian131@me.com>
2020-11-03 11:25:30 -0800

    Completed js-constructors exercise.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+var anObject = new Object();
+var newArray = new Array();
+var newFunction = new Function();
+var newNumber = new Number();
+var newBoolean = new Boolean();
+var newString = new String();
+
+console.log('value of anObject:', anObject);
+console.log('typeof anObject:', typeof anObject);
+console.log('value of newArray:', newArray);
+console.log('typeof newArray:', typeof newArray);
+console.log('value of newFunction:', newFunction);
+console.log('typeof newFunction:', typeof newFunction);
+console.log('value of newNumber:', newNumber);
+console.log('typeof newNumber:', typeof newNumber);
+console.log('value of newBoolean:', newBoolean);
+console.log('typeof newBoolean:', typeof newBoolean);
+console.log('value of newString:', newString);
+console.log('typeof newString:', typeof newString);
