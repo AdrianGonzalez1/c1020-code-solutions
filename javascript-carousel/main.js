0442b64f8fee1e6af6fb77dd6b5508db9b803ea6
Adrian Gonzalez <adrian131@me.com>
2020-11-09 19:20:44 +0000

    Save work.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,7 +1,14 @@
 var chevronRight = document.querySelector('.fa-chevron-right');
-var imgElement = document.querySelector('.pokemonImg');
-// var circleIcon = document.querySelector('.fa-circle')
+var imgElement = document.querySelector('img');
+var imgArr = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+var interval = setInterval(carousel, 3000);
+var i = 0;
+function carousel() {
+  imgElement.setAttribute('src', imgArr[i]);
+  i++;
+  if (i === 5) {
+    i = 0;
+    
+  }
 
-chevronRight.addEventListener('click', function (event) {
-  imgElement.setAttribute('src', './images/004.png');
-});
+}
